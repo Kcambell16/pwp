1f7779a9157d164c3b3fb227dcac3a3f37f55f97
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-19 22:26:53 -0600

    persona

--- milestone-1.php
+++ milestone-1.php
@@ -17,5 +17,10 @@
 		<h1> Goal</h1>
 	<p> I hope to acquire a fulltime position at a cutting edge start up company. I would also like to attract freelance clients. As an overall them I would like to really break into front end design on a more indepth level and merge my love of beauty and technology.
 
+		<!--Persona-->
+		 <h1> Persona</h1>
+	<p> Name: Bryan Nowlin CEO of Brightstars Devlopment
+		Bryan is a busy CEO of a new start up developing company named Brightstars. Bryan is looking for the perfect match. He has very specific needs, he wants to be able to hire a person who is teachable he has had experience with many senior developers and often times find them unflexable and most importantly adverse to change. Bryan wants to find someone whos is excited and eager to be a part of a fast past ever evolving team. He is looking for a "brightstar". Bryan is busy he works 12 hour days and beleieves when he see the applicants work He will just know if they are the right fit or not. He is looking for a well developed eye cathing site to give him insite on the applicants ability. Cathing Bryan's attention a first glance is very important to ensure being called in for an interview.
+
 	</p>
 </html>
