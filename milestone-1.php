deeec47f6abec57ccaed445ba2e90cd18294a0bb
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-19 21:49:19 -0600

    compelete of goal and purpose

--- milestone-1.php
+++ milestone-1.php
@@ -2,6 +2,20 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-<h2>Purpose</h2>
+
+		<!-- simple statement of purpose-->
+		<title> Personal website mockup</title>
 	</head>
+	<h1>Purpose</h1>
+	<p> The purpose of this project is provide a digital resume. I would like this site to show various aspects of the skills I have acquired. As well as display a perfect merge between the techincal and creative.    </p>
+
+	<!--Audience statment-->
+	<h1>Audience</h1>
+	<p>I plan for this site to be viewed my potential client and employers. Overall I would like to viewers to be intrigued by my work and become familar with all aspects of me.
+
+		<!--Goal statment-->
+		<h1> Goal</h1>
+	<p> I hope to acquire a fulltime position at a cutting edge start up company. I would also like to attract freelance clients. As an overall them I would like to really break into front end design on a more indepth level and merge my love of beauty and technology.
+
+	</p>
 </html>
