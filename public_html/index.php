73026b71d67d6c62ed4caf94202fbc2eb1c02c33
Kelly Campbell <Kcampbell16@cnm.edu>
2016-11-26 09:48:02 -0700

    change background pic

--- public_html/index.php
+++ public_html/index.php
@@ -37,7 +37,7 @@
 		<!-- End of Background image test -->
 
 		<header>
-			<nav class="navbar navbar-default navbar-fixed-top">
+			<nav class="navbar navbar-default navbar-fixed-bottom">
 				<div class="container-fluid">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<div class="navbar-header">
