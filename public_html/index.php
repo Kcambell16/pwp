99cc9559eea6c31aff26908e8c1b798de4fab402
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-01 09:23:43 -0700

    added recapcha output area and form and form method action to HTML

--- public_html/index.php
+++ public_html/index.php
@@ -95,6 +95,65 @@
 				</div><!-- /.container-fluid -->
 			</nav>
 		</header>
+
+		<!-- insert contact form -->
+		<form id="contact-form" method="POST" action="php/mailer.php" novalidate>
+			<div class="form-group">
+				<label for="name">Name <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-user" aria-hidden="true"></i>
+					</div>
+					<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="email">Email <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="subject">Subject</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-pencil" aria-hidden="true"></i>
+					</div>
+					<input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="message">Message <span class="text-danger">*</span></label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-comment" aria-hidden="true"></i>
+					</div>
+					<textarea class="form-control" rows="5" id="message" name="message" placeholder="Message (2000 characters max)"></textarea>
+				</div>
+			</div>
+
+			<!-- Google reCAPTCHA -->
+			<div class="g-recaptcha" data-sitekey="6LfElA0UAAAAAIPR2M15qAh5qkWlXGlRdtJw3Ud0
+"></div>
+
+			<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+			<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+		</form>
+
+
+		<!--messages output area error/success output-->
+		<div class="row">
+			<div class="col-xs-12">
+				<div id="output-area"></div>
+			</div>
+			</div>
+		</form>
+
+
+		</body>
 		<!--
 				<div class="container-fluid">
 					<div class="row">
