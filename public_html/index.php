62a5bcb018ed85f3fce07e835969dbf8eb3d4713
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-06 20:04:34 -0700

    changed layout adding pdf link

--- public_html/index.php
+++ public_html/index.php
@@ -42,63 +42,145 @@
 		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<!-- Google fonts-->
 		<link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Molle:400i" rel="stylesheet">
+
+		<!-- Custom Fonts -->
+		<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
 	</head>
 	<body>
-		<!-- Placing nav bar in container
-		<header class="navbar-inverse" role="banner">
-			<div class="container">
-				<nav role="navigation">
-					<!-- Brand and toggle get grouped for better mobile display
+		<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+			<!-- Navigation -->
+			<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+				<div class="container">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-								  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+							Menu <i class="fa fa-bars"></i>
 						</button>
-						 <img src="image/Kcampbell-website-logo.png" alt="logo">
+						<a class="navbar-brand page-scroll" href="#page-top">
+							<i class="fa fa-play-circle"></i> <span class="light">Start</span> Bootstrap
+						</a>
 					</div>
-					<div class="collpase navbar-collapse" id="example">
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse navbar-right navbar-main-collapse">
 						<ul class="nav navbar-nav">
-							<li class="active"><a href="">Websites</a></li>
-							<li class="active"><a href="">Resume</a></li>
-							<li class="active"><a href="">Contact Me</a></li>
+							<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+							<li class="hidden">
+								<a href="#page-top"></a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#about">About</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#download">Download</a>
+							</li>
+							<li>
+								<a class="page-scroll" href="#contact">Contact</a>
+							</li>
 						</ul>
-						<form action="" class="navbar-form navbar-right" role="search">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Seek">
-							</div>
-							<button type="submit" class="btn btn-danger">Submit</button>
-						</form>
 					</div>
-					<!-- Collect the nav links, forms, and other content for toggling
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav"></ul>
-						<ul class="nav navbar-nav navbar-right"></ul> ->
+					<!-- /.navbar-collapse -->
+				</div>
+				<!-- /.container -->
+			</nav>
 
-					</div> <!-- /.navbar-collapse
-				</nav>
-			</div><!-- /.container-fluid
-		</header> -->
+			<!-- Intro Header -->
+			<header class="intro">
+				<div class="intro-body">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2">
+								<h1 class="brand-heading">Grayscale</h1>
+								<p class="intro-text">A free, responsive, one page Bootstrap theme.
+									<br>Created by Start Bootstrap.</p>
+								<a href="#about" class="btn btn-circle page-scroll">
+									<i class="fa fa-angle-double-down animated"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</header>
 
-		<!-- <div class="container">
-			<div class="jumbotron" id="jumbotron" class="bg-primary">
-				<p>testing</p>
-			</div>
-		</div><!--/.container-->
+			<!-- About Section -->
+			<section id="about" class="container content-section text-center">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<h2>About Grayscale</h2>
+						<p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+						<p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
+						<p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
+					</div>
+				</div>
+			</section>
 
-		<!-- <div class="container">
-			<div class="jumbotron" class="col-sm-6" class="bk-primary">
-				Spicy jalapeno bacon ipsum dolor amet beef chicken drumstick meatball alcatra, andouille shankle ribeye
-				biltong. Chicken capicola pork loin ham pork chop bacon short loin ground round jowl porchetta corned beef
-				landjaeger hamburger shoulder pig. Kevin turkey pork belly
-			</div>
-		</div><!--/.container-->
+			<!-- Download Section -->
+			<section id="download" class="content-section text-center">
+				<div class="download-section">
+					<div class="container">
+						<div class="col-lg-8 col-lg-offset-2">
+							<h2>Download Resume</h2>
+							<p>You can download an up to date resume</p>
+							<a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<!-- Contact Section -->
+			<section id="contact" class="container content-section text-center">
+				<div class="row">
+					<div class="col-lg-8 col-lg-offset-2">
+						<h2>Contact Start Bootstrap</h2>
+						<p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
+						<p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+						</p>
+						<ul class="list-inline banner-social-buttons">
+							<li>
+								<a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+							</li>
+							<li>
+								<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+							</li>
+							<li>
+								<a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</section>
+
+			<!-- Map Section -->
+			<div id="map"></div>
+
+			<!-- Footer -->
+			<footer>
+				<div class="container text-center">
+					<p>Copyright &copy; Your Website 2016</p>
+				</div>
+			</footer>
+
+			<!-- jQuery -->
+			<script src="vendor/jquery/jquery.js"></script>
+
+			<!-- Bootstrap Core JavaScript -->
+			<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+			<!-- Plugin JavaScript -->
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+			<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+
+			<!-- Theme JavaScript -->
+			<script src="js/grayscale.min.js"></script>
 
 
-		<div class="title" >
+		<!-- <div class="title" >
 			<div>
 				<h1>Kelly Campbell</h1>
 				<h4>Web Developer</h4>
@@ -106,21 +188,28 @@
 		</div><!--/.container-->
 
 
-		<!-- Colum nesting -->
+		<!-- containers for spacing
 		<div class="row">
-			<div class="col-md-4">.col-md-4</div>
-			<div class="col-md-4 col-md-offset-4">.col-md-4 .col-md-offset-4</div>
-		</div>
-		<div class="row">
-			<div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
-			<div class="col-md-3 col-md-offset-3">.col-md-3 .col-md-offset-3</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6 col-md-offset-3">.col-md-6 .col-md-offset-3</div>
-		</div>
-
-		<!-- containers for spacing -->
-		<div class="row">
+			<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="..." alt="...">
+					<div class="caption">
+						<h3>Thumbnail label</h3>
+						<p>...</p>
+						<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-6 col-md-4">
+				<div class="thumbnail">
+					<img src="..." alt="...">
+					<div class="caption">
+						<h3>Thumbnail label</h3>
+						<p>...</p>
+						<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+					</div>
+				</div>
+			</div>
 			<div class="col-sm-6 col-md-4">
 				<div class="thumbnail">
 					<img src="..." alt="...">
@@ -133,14 +222,13 @@
 			</div>
 		</div>
 
-
-		<!-- modal fade-->
+		<!-- modal fade
 		<div class="modal fade" id="contact-form-modal" tabindex="-1" role="dialog">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-body">
 
-						<!-- insert contact form -->
+						<!-- insert contact form
 						<form id="contact-form" method="POST" action="php/mailer.php" novalidate>
 							<div id="boxhead">
 								<div class="form-group">
@@ -182,7 +270,7 @@
 									</div>
 								</div>
 
-								<!-- Google reCAPTCHA -->
+								<!-- Google reCAPTCHA
 								<div class="g-recaptcha" data-sitekey="6LfElA0UAAAAAIPR2M15qAh5qkWlXGlRdtJw3Ud0
 "></div>
 								<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send
@@ -190,20 +278,21 @@
 								<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
 						</form>
 
-						<!--messages output area error/success output-->
+						<!--messages output area error/success output
 						<div class="row">
 							<div class="col-xs-12">
 								<div id="output-area"></div>
 							</div>
 						</div>
-					</div><!-- /.modal-content -->
-				</div><!-- /.modal-dialog -->
-			</div><!-- /.modal -->
+					</div><!-- /.modal-content
+				</div><!-- /.modal-dialog
+			</div><!-- /.modal
 		</div>
 		</div>
 
-		<!-- footer -->
-		<nav class="navbar navbar-inverse navbar-bottom">
+
+		<!-- footer
+		<nav class="navbar navbar-inverse navbar-fixed-bottom">
 			<div class="container">
 				<div class="navbar-header">
 					<a class="navbar-brand" data-toggle="modal" data-target="#contact-form-modal">CONTACT ME</a>
