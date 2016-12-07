b81f471ae3fc16071620ac719f5d1cfbf4800111
Kelly Campbell <Kcampbell16@cnm.edu>
2016-12-06 21:24:42 -0700

    css nav bar

--- public_html/index.php
+++ public_html/index.php
@@ -46,7 +46,8 @@
 
 		<!-- Custom Fonts -->
 		<link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
+				type="text/css">
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 
 	</head>
@@ -57,11 +58,12 @@
 			<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
 				<div class="container">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+						<button type="button" class="navbar-toggle" data-toggle="collapse"
+								  data-target=".navbar-main-collapse">
 							Menu <i class="fa fa-bars"></i>
 						</button>
 						<a class="navbar-brand page-scroll" href="#page-top">
-							<i class="fa fa-play-circle"></i> <span class="light">Start</span> Bootstrap
+							<i class="fa fa-play-circle"></i>
 						</a>
 					</div>
 
@@ -94,9 +96,8 @@
 					<div class="container">
 						<div class="row">
 							<div class="col-md-8 col-md-offset-2">
-								<h1 class="brand-heading">Grayscale</h1>
-								<p class="intro-text">A free, responsive, one page Bootstrap theme.
-									<br>Created by Start Bootstrap.</p>
+								<h1 class="brand-heading">Kelly Campbell</h1>
+								<p class="intro-text">Welcome to my journey as a Web Developer</p>
 								<a href="#about" class="btn btn-circle page-scroll">
 									<i class="fa fa-angle-double-down animated"></i>
 								</a>
@@ -110,10 +111,9 @@
 			<section id="about" class="container content-section text-center">
 				<div class="row">
 					<div class="col-lg-8 col-lg-offset-2">
-						<h2>About Grayscale</h2>
-						<p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
-						<p>This theme features stock photos by <a href="http://gratisography.com/">Gratisography</a> along with a custom Google Maps skin courtesy of <a href="http://snazzymaps.com/">Snazzy Maps</a>.</p>
-						<p>Grayscale includes full HTML, CSS, and custom JavaScript files along with LESS files for easy customization.</p>
+						<h2>About Kelly</h2>
+						<p>I am a full stack web developer with a passion for beauty! Front end UI/UX is my focus, however I do enjoy the process of data design and other back end development. My background is fashion design. While pursuing my degree in fashion design I was bit by the Dev bug. As a senior requirement all design majors were required to create an online portfolio displaying our senior collection. We then used dream weaver, at the time I had no idea exactly what I was getting into. I did know however, I liked it and most of all I like combining my love of the arts and technology. Fast forward a few years after completing my degree at The Art Institute of Philadelphia I am happy my journey has lead me down the path of being a full stack developer. <a href="https://Cannaduceus.com/public_html">the
+								Click here</a>.
 					</div>
 				</div>
 			</section>
@@ -125,29 +125,98 @@
 						<div class="col-lg-8 col-lg-offset-2">
 							<h2>Download Resume</h2>
 							<p>You can download an up to date resume</p>
-							<a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+							<a href="image/resume.pdf" class="btn btn-default btn-lg">download</a>
 						</div>
 					</div>
 				</div>
 			</section>
 
+			<!-- modal fade -->
+			<div class="modal fade" id="contact-form-modal" tabindex="-1" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-body">
+
+							<!-- insert contact form-->
+							<form id="contact-form" method="POST" action="php/mailer.php" novalidate>
+								<div id="boxhead">
+									<div class="form-group">
+										<label for="name">Name <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-user" aria-hidden="true"></i>
+											</div>
+											<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="email">Email <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-envelope" aria-hidden="true"></i>
+											</div>
+											<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="subject">Subject</label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-pencil" aria-hidden="true"></i>
+											</div>
+											<input type="text" class="form-control" id="subject" name="subject"
+													 placeholder="Subject">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="message">Message <span class="text-danger">*</span></label>
+										<div class="input-group">
+											<div class="input-group-addon">
+												<i class="fa fa-comment" aria-hidden="true"></i>
+											</div>
+											<textarea class="form-control" rows="5" id="message" name="message"
+														 placeholder="Message (2000 characters max)"></textarea>
+										</div>
+									</div>
+
+									<!-- Google reCAPTCHA -->
+									<div class="g-recaptcha" data-sitekey="6LfElA0UAAAAAIPR2M15qAh5qkWlXGlRdtJw3Ud0">
+
+									</div>
+									<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane">
+										</i> Send
+									</button>
+									<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+							</form>
+
+							<!--messages output area error/success output -->
+							<div class="row">
+								<div class="col-xs-12">
+									<div id="output-area"></div>
+								</div>
+							</div>
+						</div><!-- /.modal-content -->
+					</div><!-- /.modal-dialog -->
+				</div><!-- /.modal -->
+			</div>
+			</div>
+
 			<!-- Contact Section -->
 			<section id="contact" class="container content-section text-center">
 				<div class="row">
 					<div class="col-lg-8 col-lg-offset-2">
-						<h2>Contact Start Bootstrap</h2>
-						<p>Feel free to email us to provide some feedback on our templates, give us suggestions for new templates and themes, or to just say hello!</p>
-						<p><a href="mailto:feedback@startbootstrap.com">feedback@startbootstrap.com</a>
+						<h2>Contact me</h2>
+						<p>Feel free to contact me via email and also follow me on social media.</p>
+						<p><a data-toggle="modal" data-target="#contact-form-modal">Send me a message</a></a>
 						</p>
 						<ul class="list-inline banner-social-buttons">
 							<li>
-								<a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+								<a href="https://twitter.com/PrettyDeveloper" class="btn btn-default btn-lg"><i
+										class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
 							</li>
 							<li>
-								<a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
-							</li>
-							<li>
-								<a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i> <span class="network-name">Google+</span></a>
+								<a href="https://github.com/Kcambell16" class="btn btn-default btn-lg"><i
+										class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
 							</li>
 						</ul>
 					</div>
@@ -174,137 +243,10 @@
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 
 			<!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
-			<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+			<script type="text/javascript"
+					  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
 
 			<!-- Theme JavaScript -->
 			<script src="js/grayscale.min.js"></script>
 
 
-		<!-- <div class="title" >
-			<div>
-				<h1>Kelly Campbell</h1>
-				<h4>Web Developer</h4>
-			</div>
-		</div><!--/.container-->
-
-
-		<!-- containers for spacing
-		<div class="row">
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="..." alt="...">
-					<div class="caption">
-						<h3>Thumbnail label</h3>
-						<p>...</p>
-						<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="..." alt="...">
-					<div class="caption">
-						<h3>Thumbnail label</h3>
-						<p>...</p>
-						<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-6 col-md-4">
-				<div class="thumbnail">
-					<img src="..." alt="...">
-					<div class="caption">
-						<h3>Thumbnail label</h3>
-						<p>...</p>
-						<p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- modal fade
-		<div class="modal fade" id="contact-form-modal" tabindex="-1" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-body">
-
-						<!-- insert contact form
-						<form id="contact-form" method="POST" action="php/mailer.php" novalidate>
-							<div id="boxhead">
-								<div class="form-group">
-									<label for="name">Name <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-user" aria-hidden="true"></i>
-										</div>
-										<input type="text" class="form-control" id="name" name="name" placeholder="Name">
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="email">Email <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-envelope" aria-hidden="true"></i>
-										</div>
-										<input type="email" class="form-control" id="email" name="email" placeholder="Email">
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="subject">Subject</label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-pencil" aria-hidden="true"></i>
-										</div>
-										<input type="text" class="form-control" id="subject" name="subject"
-												 placeholder="Subject">
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="message">Message <span class="text-danger">*</span></label>
-									<div class="input-group">
-										<div class="input-group-addon">
-											<i class="fa fa-comment" aria-hidden="true"></i>
-										</div>
-										<textarea class="form-control" rows="5" id="message" name="message"
-													 placeholder="Message (2000 characters max)"></textarea>
-									</div>
-								</div>
-
-								<!-- Google reCAPTCHA
-								<div class="g-recaptcha" data-sitekey="6LfElA0UAAAAAIPR2M15qAh5qkWlXGlRdtJw3Ud0
-"></div>
-								<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send
-								</button>
-								<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
-						</form>
-
-						<!--messages output area error/success output
-						<div class="row">
-							<div class="col-xs-12">
-								<div id="output-area"></div>
-							</div>
-						</div>
-					</div><!-- /.modal-content
-				</div><!-- /.modal-dialog
-			</div><!-- /.modal
-		</div>
-		</div>
-
-
-		<!-- footer
-		<nav class="navbar navbar-inverse navbar-fixed-bottom">
-			<div class="container">
-				<div class="navbar-header">
-					<a class="navbar-brand" data-toggle="modal" data-target="#contact-form-modal">CONTACT ME</a>
-				</div>
-				<div>
-					<ul class="nav navbar-nav navbar-right" id="navbar">
-						<li><a>Kcampbell5784@gmail.com</a></li>
-						<li><a>(484)-358-2153</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-
-	</body>
-</html>
